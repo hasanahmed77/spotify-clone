2b30fbcc3b9a735488f9063eba28e3a0c8bcc5b1
Mustakim Ahmed Hasan <mustakimahmedhasan@Mustakims-MacBook-Air.local>
2024-03-19 22:59:11 +0600

    the app now can GET playlist after authentication

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import Login from './Login';
 import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from 'spotify-web-api-js'
+
+const spotify = new SpotifyWebApi()
 
 function App() {
   const [token, setToken] = useState()
@@ -13,10 +16,27 @@ function App() {
     window.location.hash = ""
     const _token = hash.access_token
 
-    if(_token) setToken(_token)
+    if(_token) {
+      setToken(_token)
+      spotify.setAccessToken(_token)
+    }
 
     console.log("token", token)
   }, [])
+
+  if (token) {
+    fetch(`https://api.spotify.com/v1/playlists/${process.env.REACT_APP_PLAYLIST}`, {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+    })
+    .catch(err => console.log('error', err))
+  }
 
   return (
     <div className="App">
